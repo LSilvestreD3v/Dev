e8867badff78725ac4f97d4431a6a50e9f2987ea
Lucas Silvestre <88158275+LSilvestreD3v@users.noreply.github.com>
2024-01-18 23:20:39 +0000

    yes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,15 @@
 alert('Bem Vindo ao Número Misterio');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute = prompt('Digite um numero entre 1 a 10');
+console.log(numeroSecreto);
+let chute ;
+let tentativas = 1;
 
-// se chute for igual ao número secreto
+// enaquanto chute nao for igual a n.s
+while (chute != numeroSecreto) {
+    chute = prompt('Digite um numero entre 1 a 10');
+    // se chute for igual ao número secreto
 if(chute == numeroSecreto) {
-    alert(`Boa! Você acertou ${numeroSecreto}`);
+    alert(`Boa! Você acertou ${numeroSecreto} com ${tentativas} tentativas.`);
 } else{
     if (chute >numeroSecreto ) {
         alert(`número secreto é menor que ${chute}`);
@@ -13,3 +17,8 @@ if(chute == numeroSecreto) {
         alert(`número secreto é maior que ${chute}`);
     }
 };
+    //tentativas = tentativas +1;
+    tentativas++
+}
+}
+ 
