12ad68c91f64cf8c5ef595334ed93b8f24cc8126
Lucas Silvestre <88158275+LSilvestreD3v@users.noreply.github.com>
2024-01-18 23:32:35 +0000

    Finalização - Numero Secreto
    ultima etapa do jogo de advinhação, logica basica de programaçao com JS

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,23 +1,26 @@
-alert('Bem Vindo ao Número Misterio');
-let numeroSecreto = 5;
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 500
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
-let chute ;
+let chute;
 let tentativas = 1;
 
-// enaquanto chute nao for igual a n.s
+// enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Digite um numero entre 1 a 10');
-    // se chute for igual ao número secreto
-if(chute == numeroSecreto) {
-    alert(`Boa! Você acertou ${numeroSecreto} com ${tentativas} tentativas.`);
-} else{
-    if (chute >numeroSecreto ) {
-        alert(`número secreto é menor que ${chute}`);
-    } else{
-        alert(`número secreto é maior que ${chute}`);
-    }
-    //tentativas = tentativas +1;
-    tentativas++
+chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+break;
+} else {
+if (chute > numeroSecreto) {
+alert(`O número secreto é menor que ${chute}`)
+} else {
+alert(`O número secreto é maior que ${chute}`)
+}
+// tentativas = tentativas + 1
+tentativas++
 }
 }
- 
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
