4d65be08fc7f4ac1bbf423a58c341f4153309633
Lucas Silvestre <88158275+LSilvestreD3v@users.noreply.github.com>
2024-01-19 09:33:30 +0000

    Update apptreino.js
    treinos

--- logica-js-projeto_inicial/Primeira-Logica/apptreino.js
+++ logica-js-projeto_inicial/Primeira-Logica/apptreino.js
@@ -1,20 +1,6 @@
-alert('Bem Vindo ao Site');
-
-let nome = 'Lua';
-let Idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-
-alert(Erro! Preencha os camposDeErro);
-
-let mensagemDeErro = 'Erro! Preencha os campos';
-alert(mensagemDeErro)
-
-let nome = prompt('Digite seu nome');
-let idade = prompt('Digite sua Idade');
-
-let idade = prompt('digite sua idade');
-if (idade >= 18) {
-    alert('pode conduzir!');
-}
-
+let nome = Lucas
+let sobrenome = Silvestre
+let pergunta = prompt('qual seu nome');
+let pergunta2 = prompt('qual seu sobrenome');
+let nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
