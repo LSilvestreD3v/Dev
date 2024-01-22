281bb4daf34c5f3a17abbb88ce7b5ea32e8a27bd
Lucas Silvestre <88158275+LSilvestreD3v@users.noreply.github.com>
2024-01-22 13:15:05 +0000

    Update app.js
    passo 3

--- logica-js-projeto_inicial/logica-js-html/app.js
+++ logica-js-projeto_inicial/logica-js-html/app.js
@@ -1,18 +1,18 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = gerarNumeroAleatorio(); // Função para gerar numero
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1','Desafio Secreto')
-exibirTextoNaTela('p','escolha um numero de 1 a 10');
+exibirTextoNaTela('h1','Desafio Secreto') //funçao de exibiçao
+exibirTextoNaTela('p','escolha um numero de 1 a 10'); //funcao de exibiçao
 
-function verificarChute(){
+function verificarChute(){ // funçao de verificar 
     let chute = document.querySelector('input'). value;
     console.log(chute == numeroSecreto);
 }
 
-function gerarNumeroAleatorio(params) {
+function gerarNumeroAleatorio(params) { //comando para gerar numero e retorna
     return parseInt(Math.random() * 10 + 1)
 }
